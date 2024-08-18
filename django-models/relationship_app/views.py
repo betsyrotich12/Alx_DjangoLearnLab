82b215fa55ff3cad1ceb58b7037ddc0410c3dd75
Betsy Rotich <betsyrotich022@gmail.com>
2024-08-18 14:41:38 +0300

    Implementing Custom Permissions in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,8 +3,10 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import Book, Library
+
 
 # View to list all books
 def list_books(request):
