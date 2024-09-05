38cad06fd24448b2704b5a1e6232dd65418cdfc7
Betsy Rotich <betsyrotich022@gmail.com>
2024-09-05 20:45:20 +0300

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from django.db import models
+from rest_framework import serializers
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title', 'publication_year', 'author']
+
+    def validate_publication_year(self, value):
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be a future date")
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
+
+
+
+
