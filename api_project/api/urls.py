a3ab6217af22f133aab55b8d5ef9f4ab7fea7c70
Betsy Rotich <betsyrotich022@gmail.com>
2024-09-05 22:58:08 +0300

    Building Your First API Endpoint with Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookViewSet
+from .views import BookList
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
@@ -9,4 +10,5 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,9 +2,10 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()  # Retrieve all books
     serializer_class = BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
