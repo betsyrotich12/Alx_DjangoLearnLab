185cb412bf355c14b8b1b199fa3b0a64a9a2f43e
Betsy Rotich <betsyrotich022@gmail.com>
2024-09-07 21:26:30 +0300

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters import rest_framework as django_filters
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import mixins
