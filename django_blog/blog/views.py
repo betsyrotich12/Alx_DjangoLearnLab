5538e04f0e48e63e8275cb82d4358d7b93f5ba43
Betsy Rotich <betsyrotich022@gmail.com>
2024-09-14 20:17:18 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render 
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import View
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'blog/register.html', {'form':form})
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render(request, 'blog/register.html', {'form':form})
+    
+@login_required
+def profile_view(request):
+    return render(request, 'blog/profile.html', {'user':request.user})
+
+@login_required
+def profile_edit_view(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.Post, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+        else:
+            form = ProfileForm(instance=request.user.profile)
+        return render(request, 'blog/profile_edit.html', {'form':form})    
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -80,11 +80,8 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'root',
-        'PASSWORD': 'Betsyme234!',
-        'HOST': 'localhost',
-        'PORT': '3307',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
