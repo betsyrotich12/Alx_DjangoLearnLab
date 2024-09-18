332129b668ac5b979b712f3be777e7bbec91de78
Betsy Rotich <betsyrotich022@gmail.com>
2024-09-18 19:53:49 +0300

    Implementing Posts and Comments Functionality

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
+    title = models.TextField(max_length=50)
     content = models.TextField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
